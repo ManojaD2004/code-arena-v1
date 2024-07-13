476bc82ffcd377bfe4f7e2637b3b2636b13b9f59
ManojaD2004 <manojadkc2004@gmail.com>
2024-07-13 10:35:29 +0530

    First v3

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+// const dotenv = require("dotenv");
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/home", (req,res) => {
+    try {
+        res.status(200);
+        res.send("Hello Tiger");
+    } catch (error) {
+        console.log(error);
+        res.status(400);
+        res.send(error);
+    }
+})
+
+app.listen(PORT, () => {
+  console.log(`Listening to Port: ${PORT}`);
+});
